303627d7177bf9fba99f992a0ddc3563f290556a
Sylvie <liqinglq666@gmail.com>
2026-08-08 13:57:15 +0900

    style: load judge experience styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import './editorial-try.css';
 import './editorial-workspace.css';
 import './editorial-review.css';
 import './editorial-responsive.css';
+import './judge.css';
 import './professional-ui.css';
 import './professional-ui-fixes.css';
 
